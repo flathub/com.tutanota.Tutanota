4dd6c1af9d13c4f7612458cd0310b3e82b4d8544
nig <nig@tutao.de>
2020-09-24 12:46:44 +0200

    update to tutanota 3.76.10

--- generate.bash
+++ generate.bash
@@ -2,20 +2,32 @@
 
 set -eu
 
-TAG=tutanota-release-3.50.10
-
-export cache="${PWD}/generate-cache"
-rm -rf "${cache}"
-mkdir -p "${cache}"
-
-[ -d tutanota ] || git clone https://github.com/tutao/tutanota.git
+# get the correct commit from the tutanota repo
+[ -d tutanota ] || git clone https://github.com/ganthern/tutanota.git
 cd tutanota
+TAG=`git describe --tags --abbrev=0 --match "flatpak*" HEAD`
+VERSION=`echo ${TAG} | tail -c 8`
+ARCHIVE="tutanota-desktop-${VERSION}-unpacked-linux.tar.gz"
+URL="https://github.com/ganthern/tutanota/releases/download/${TAG}/${ARCHIVE}"
 git fetch
 git checkout -f "${TAG}"
-patch -p1 <../generate.patch
-python3 ../flatpak-npm-generator.py package-lock.json -o ../generated-sources.json
+
+# make a build to get the package-lock of the desktop client
+# it's copied to the root of the repo automatically
+echo $VERSION
+echo $URL
+echo $TAG
 npm install
-export npm_config_cache="${cache}"
-node dist release -l
-python3 ../flatpak-npm-generator.py build/dist/package-lock.json -o ../dist-generated-sources.json
-cp build/dist/package-lock.json ..
+node dist --unpacked --custom-desktop-release
+cd ..
+
+# tar up the client
+tar -czvf ${ARCHIVE} ./build/desktop/linux-unpacked
+
+# get checksum
+CHECKSUM=(`sha256sum ${ARCHIVE}`)
+
+# generate manifest
+
+# clean up
+rm -rf ./.idea ./generate-cache ./tutanota
