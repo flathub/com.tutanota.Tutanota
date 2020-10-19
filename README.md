910acdccbec0cd123e0421a4995b644f446d83de
nig <nig@tutao.de>
2020-10-19 09:02:32 +0200

    automate updating

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-In order to update:
+## In order to update:
 
-- Change tag in `generate.bash`.
+- Change tag in `generate.bash` to the version to be built
 - Run `./generate.bash`.
-- Update tag and commit in `com.tutanota.Tutanota.json`.
 - Update changelog in `com.tutanota.Tutanota.appdata.xml`.
+
+
+## Modules
+
+- python2 from shared_modules: apparently required for flatpak-builder to run
+- libsecret from shared_modules: used by keytar to access keyrings
+- node: to run npm
+- tutanota: our app
+- metadata: 
---
+++ manifest-template.js
@@ -0,0 +1,85 @@
+#!/bin/node
+const fs = require('fs')
+
+const [hash, url] = process.argv.slice(2)
+
+const manifest = {
+    "app-id": "com.tutanota.Tutanota",
+    "runtime": "org.freedesktop.Platform",
+    "runtime-version": "19.08",
+    "base": "org.electronjs.Electron2.BaseApp",
+    "base-version": "19.08",
+    "branch": "stable",
+    "sdk": "org.freedesktop.Sdk",
+    "command": "tutanota-desktop",
+    "separate-locales": false,
+    "finish-args": [
+        "--share=ipc",
+        "--socket=x11",
+        "--socket=wayland",
+        "--socket=pulseaudio",
+        "--share=network",
+        "--filesystem=host",
+        "--talk-name=org.freedesktop.Notifications",
+        "--talk-name=org.freedesktop.secrets",
+        "--talk-name=org.kde.StatusNotifierWatcher",
+        "--env=TMPDIR=/var/tmp"
+    ],
+    "modules": [
+        "shared-modules/libsecret/libsecret.json",
+        {
+            "name": "tutanota",
+            "buildsystem": "simple",
+            "sources": [
+                {
+                    "dest-filename": "tutanota-unpacked-linux.tar.gz",
+                    "type": "file",
+                    "sha256": hash,
+                    "url": url
+                },
+                {
+                    "type": "script",
+                    "dest-filename": "tutanota-desktop.sh",
+                    "commands": [
+                        "export TMPDIR=\"$XDG_RUNTIME_DIR/app/${FLATPAK_ID}\"",
+                        "exec zypak-wrapper /app/lib/tutanota/tutanota-desktop \"$@\""
+                    ]
+                }
+            ],
+            "build-commands": [
+                "tar -xf tutanota-unpacked-linux.tar.gz",
+                "rm tutanota-unpacked-linux.tar.gz",
+                "mkdir -p /app/lib/",
+                "cp -r linux-unpacked /app/lib/tutanota",
+                "install -Dm644 linux-unpacked/resources/icons/icon/512.png /app/share/icons/hicolor/512x512/apps/com.tutanota.Tutanota.png",
+                "install -Dm644 linux-unpacked/resources/icons/icon/64.png /app/share/icons/hicolor/64x64/apps/com.tutanota.Tutanota.png",
+                "install -Dm755 tutanota-desktop.sh /app/bin/tutanota-desktop",
+                "rm -rf ./linux-unpacked"
+            ]
+        },
+        {
+            "name": "metadata",
+            "buildsystem": "simple",
+            "sources": [
+                {
+                    "type": "file",
+                    "path": "com.tutanota.Tutanota.appdata.xml"
+                },
+                {
+                    "type": "file",
+                    "path": "com.tutanota.Tutanota.desktop"
+                }
+            ],
+            "build-commands": [
+                "install -Dm644 com.tutanota.Tutanota.appdata.xml /app/share/appdata/com.tutanota.Tutanota.appdata.xml",
+                "install -Dm644 com.tutanota.Tutanota.desktop /app/share/applications/com.tutanota.Tutanota.desktop"
+            ]
+        }
+    ]
+}
+
+try {
+    fs.unlinkSync('com.tutanota.Tutanota.json')
+} catch {
+}
+fs.writeFileSync('com.tutanota.Tutanota.json', JSON.stringify(manifest, null, 2))
