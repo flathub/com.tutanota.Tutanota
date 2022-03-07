134a3d2a2754cbd6046d4bf4d474a7ef66a7b2ab
jom <jom@tutao.de>
2022-03-07 15:46:08 +0100

    update to vrelease-3.93.5

--- generate.bash
+++ generate.bash
@@ -6,11 +6,11 @@ set -eu
 [ -d tutanota ] || git clone --depth 20 https://github.com/tutao/tutanota.git
 cd tutanota
 git fetch
-TAG=`git describe --tags $(git rev-list --tags --max-count=1) --match "tutanota-release-*"`
-if [[ $TAG == tutanota-release-* ]]; then
+TAG=`git describe --tags $(git rev-list --tags --max-count=1) --match "tutanota-desktop-release-*"`
+if [[ $TAG == tutanota-desktop-release-* ]]; then
    echo $TAG
  else
-   echo "tag ${TAG} doesn't match tutanota-release-*"
+   echo "tag ${TAG} doesn't match tutanota-desktop-release-*"
    exit 1
 fi
 VERSION=`echo ${TAG:17}`
@@ -23,7 +23,7 @@ git checkout -f "${TAG}"
 # generate the client
 npm install
 npm run build-packages
-node dist --unpacked --custom-desktop-release
+node desktop --unpacked --custom-desktop-release
 cd ..
 
 echo "packing client"
